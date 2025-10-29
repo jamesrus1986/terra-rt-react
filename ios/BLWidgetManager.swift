eead1102cd2532929423a9e77b5615af04c340d8
JR <jr@upzelo.com>
2025-10-29 12:57:58 +0000

    Wrap BLWidget, for RN New Arch compatibility

--- ios/BLWidgetManager.swift
+++ ios/BLWidgetManager.swift
@@ -14,46 +14,133 @@ import UIKit
 
 @available(iOS 13.0, *)
 @objc(BLWidget)
-class BLWidget: UIView{
-    var connection_: Connections? = nil
-    
-    lazy var widgetView: TerraBLEWidget? = {
-        guard let terraRT = TerraRtReact.terraRt else{
-            return nil
-        }
-        return terraRT.startBluetoothScan(type: .BLE, bluetoothLowEnergyFromCache: self.withCache){success in
-            if let successCallback = self.onSuccessfulConnection {
-                successCallback(["success": success])
-            }
-            DispatchQueue.main.async {
-                let viewController = UIApplication.shared.delegate?.window??.rootViewController
-                viewController?.dismiss(animated: true)
+class BLWidget: UIView {
+    private var hostingController: UIHostingController<TerraBLEWidget>?
+    private var widgetContent: TerraBLEWidget?
+
+    @objc var withCache: Bool = false {
+        didSet {
+            if oldValue != withCache {
+                tearDownWidget()
+                scheduleSetup()
             }
         }
-    }()
-    
-    @objc var withCache: Bool = false
-    
+    }
+
     @objc var onSuccessfulConnection: RCTDirectEventBlock?
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setupView()
+        commonInit()
     }
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
+        commonInit()
     }
-    
-    func embeddedView() -> UIView{
-        return UIHostingController(rootView: widgetView).view!
+
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        if window != nil {
+            scheduleSetup()
+        } else {
+            tearDownWidget()
+        }
     }
-    
-    func setupView(){
-        let uiHost = UIHostingController(rootView: widgetView)
-        let viewController = UIApplication.shared.delegate?.window??.rootViewController
-        viewController?.present(uiHost, animated: true)
+
+    private func commonInit() {
+        backgroundColor = .clear
+    }
+
+    private func scheduleSetup() {
+        DispatchQueue.main.async { [weak self] in
+            self?.setupWidgetIfPossible()
+        }
+    }
+
+    private func setupWidgetIfPossible(retryCount: Int = 0) {
+        guard hostingController == nil else { return }
+        guard let widget = makeWidgetContent() else {
+            if retryCount < 5 {
+                // Short retries give TerraRT time to finish initialising.
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+                    self?.setupWidgetIfPossible(retryCount: retryCount + 1)
+                }
+            }
+            return
+        }
+
+        let controller = UIHostingController(rootView: widget)
+        controller.view.backgroundColor = .clear
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(controller.view)
+        NSLayoutConstraint.activate([
+            controller.view.topAnchor.constraint(equalTo: topAnchor),
+            controller.view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            controller.view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            controller.view.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+
+        if let parent = findParentViewController() {
+            parent.addChild(controller)
+            controller.didMove(toParent: parent)
+        }
+
+        hostingController = controller
+    }
+
+    private func makeWidgetContent() -> TerraBLEWidget? {
+        guard let terraRT = TerraRtReact.terraRt else {
+            return nil
+        }
+
+        widgetContent = terraRT.startBluetoothScan(
+            type: .BLE,
+            bluetoothLowEnergyFromCache: withCache
+        ) { [weak self] success in
+            guard let self = self else { return }
+            if let callback = self.onSuccessfulConnection {
+                callback(["success": success])
+            }
+            DispatchQueue.main.async {
+                self.tearDownWidget()
+            }
+        }
+
+        return widgetContent
+    }
+
+    private func tearDownWidget() {
+    guard let controller = hostingController else { return }
+
+        let parent = controller.parent
+        if parent != nil {
+            controller.willMove(toParent: nil)
+        }
+        controller.view.removeFromSuperview()
+        if let parent = parent {
+            controller.removeFromParent()
+            parent.view.setNeedsLayout()
+        }
+
+        hostingController = nil
+        widgetContent = nil
+    }
+
+    private func findParentViewController() -> UIViewController? {
+        var responder: UIResponder? = self
+        while let next = responder?.next {
+            if let viewController = next as? UIViewController {
+                return viewController
+            }
+            responder = next
+        }
+        return nil
+    }
+
+    deinit {
+        tearDownWidget()
     }
 }
 
