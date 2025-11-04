999906d84f865ee1d3bf5e376d2fa3518f79e3ff
JR <jr@upzelo.com>
2025-11-04 12:28:38 +0000

    fix: use codegen component directly to avoid duplicate view registration

--- src/BLWidget.tsx
+++ src/BLWidget.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Platform, requireNativeComponent, type ViewProps } from 'react-native';
-import type { SuccessfulConnectionEvent } from './BLWidgetNativeComponent';
+import {
+  Platform,
+  type ViewProps,
+  type NativeSyntheticEvent,
+} from 'react-native';
+import BLWidgetNativeComponent, {
+  type SuccessfulConnectionEvent,
+} from './BLWidgetNativeComponent';
 
 export interface BLWidgetProps extends ViewProps {
   withCache?: boolean;
   onSuccessfulConnection?: (event: SuccessfulConnectionEvent) => void;
 }
 
-const BLWidgetNative =
-  Platform.OS === 'ios'
-    ? requireNativeComponent<BLWidgetProps>('BLWidget')
-    : () => null;
-
-export const BLWidget: React.FC<BLWidgetProps> = (props) => {
+export const BLWidget: React.FC<BLWidgetProps> = ({
+  onSuccessfulConnection,
+  ...props
+}) => {
   if (Platform.OS !== 'ios') {
     return null;
   }
 
-  return <BLWidgetNative {...props} />;
+  const handleSuccessfulConnection = (
+    event: NativeSyntheticEvent<SuccessfulConnectionEvent>
+  ) => {
+    if (onSuccessfulConnection) {
+      onSuccessfulConnection(event.nativeEvent);
+    }
+  };
+
+  return (
+    <BLWidgetNativeComponent
+      {...props}
+      onSuccessfulConnection={handleSuccessfulConnection}
+    />
+  );
 };
